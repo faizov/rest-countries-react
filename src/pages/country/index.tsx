ea6d0befc6bcf547aa2b0cc9de897e8c53f2d13d
Yuriy <yuriy.faizov@gmail.com>
2022-11-05 13:57:27 +0300

    Add check info country page

--- src/pages/country/index.tsx
+++ src/pages/country/index.tsx
@@ -18,9 +18,10 @@ export const Country = () => {
   const { data: CountriesCode } = useGetCountriesByCodeQuery(country?.borders);
 
   const nativeName =
-    country && Object.values(country?.name?.nativeName)[0].official;
-  const currencies = country && Object.values(country?.currencies);
-  const languages = country && Object.values(country?.languages);
+    country?.name?.nativeName &&
+    Object.values(country?.name?.nativeName)[0].official;
+  const currencies = country?.currencies && Object.values(country?.currencies);
+  const languages = country?.languages && Object.values(country?.languages);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -42,67 +43,83 @@ export const Country = () => {
         <p>Back</p>
       </button>
       <div className="country-page-info">
-        <div className="country-page-info-image">
-          <img src={country?.flags.svg} alt="" />
-        </div>
+        {country?.flags.svg && (
+          <div className="country-page-info-image">
+            <img src={country?.flags.svg} alt="" />
+          </div>
+        )}
         <div className="country-page-info-details">
           <div>
             <h1>{country?.name.common}</h1>
             <div className="country-page-info-details-lists">
               <ul>
-                <li>
-                  <h4>Native Name: </h4>
-                  <p>{nativeName}</p>
-                </li>
-                <li>
-                  <h4>Population: </h4>
-                  <p>{country?.population}</p>
-                </li>
-                <li>
-                  <h4>Region: </h4>
-                  <p>{country?.region}</p>
-                </li>
-                <li>
-                  <h4>Sub Region: </h4>
-                  <p>{country?.subregion}</p>
-                </li>
-                <li>
-                  <h4>Capital: </h4>
-                  {country?.capital &&
-                    country?.capital
+                {nativeName && (
+                  <li>
+                    <h4>Native Name: </h4>
+                    <p>{nativeName}</p>
+                  </li>
+                )}
+                {country?.population && (
+                  <li>
+                    <h4>Population: </h4>
+                    <p>{country?.population}</p>
+                  </li>
+                )}
+                {country?.region && (
+                  <li>
+                    <h4>Region: </h4>
+                    <p>{country?.region}</p>
+                  </li>
+                )}
+                {country?.subregion && (
+                  <li>
+                    <h4>Sub Region: </h4>
+                    <p>{country?.subregion}</p>
+                  </li>
+                )}
+                {country?.capital && (
+                  <li>
+                    <h4>Capital: </h4>
+                    {country?.capital
                       .map<React.ReactNode>((item) => {
                         return <p key={item}>{item}</p>;
                       })
                       .reduce((prev, curr) => [prev, ",", curr])}
-                </li>
+                  </li>
+                )}
               </ul>
 
               <ul>
-                <li>
-                  <h4>Top Level Domain: </h4>
-                  {country?.tld &&
-                    country?.tld
+                {country?.tld && (
+                  <li>
+                    <h4>Top Level Domain: </h4>
+                    {country?.tld
                       .map<React.ReactNode>((item) => {
                         return <p key={item}>{item}</p>;
                       })
                       .reduce((prev, curr) => [prev, ",", curr])}
-                </li>
-                <li>
-                  <h4>Currencies: </h4>
-                  {currencies
-                    ?.map<React.ReactNode>((item) => {
-                      return <p key={item.name}>{item.name}</p>;
-                    })
-                    .reduce((prev, curr) => [prev, ",", curr])}
-                </li>
-                <li>
-                  <h4>Languages: </h4>
-                  {languages
-                    ?.map<React.ReactNode>((item) => {
-                      return <p key={item}>{item}</p>;
-                    })
-                    .reduce((prev, curr) => [prev, ",", curr])}
-                </li>
+                  </li>
+                )}
+                {currencies && (
+                  <li>
+                    <h4>Currencies: </h4>
+                    {currencies
+                      ?.map<React.ReactNode>((item) => {
+                        return <p key={item.name}>{item.name}</p>;
+                      })
+                      .reduce((prev, curr) => [prev, ",", curr])}
+                  </li>
+                )}
+                {languages && (
+                  <li>
+                    <h4>Languages: </h4>
+                    {languages
+                      ?.map<React.ReactNode>((item) => {
+                        return <p key={item}>{item}</p>;
+                      })
+                      .reduce((prev, curr) => [prev, ",", curr])}
+                  </li>
+                )}
               </ul>
             </div>
             {CountriesCode && (
